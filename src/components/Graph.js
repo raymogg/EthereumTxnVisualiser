5083da52f5354e1c12966b18f1d841a005e80fe8
raymogg <raymondmogg@gmail.com>
2019-05-05 12:12:02 +1000

    Added placeholder components

---
+++ src/components/Graph.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class Graph extends Component {
+    state = {
+        
+    };
+
+    componentDidMount = async () => {
+        
+    }
+
+    render() {
+        return (
+            <h1> This is the graph </h1>
+        )
+    }
+}
+
+export default (Graph);
+
