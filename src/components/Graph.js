7f346290da28b7a8d247bbd8f35ef2b7bde99cd2
raymogg <raymondmogg@gmail.com>
2019-05-05 13:38:27 +1000

    Removed sigma. Added graphing using react d3 tree

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -3,6 +3,37 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField';
+import { Sigma, RandomizeNodePositions, RelativeSize } from 'react-sigma'
+import Tree from 'react-d3-tree';
+
+const myTreeData = [
+    {
+        name: 'Top Level',
+        attributes: {
+            keyA: 'val A',
+            keyB: 'val B',
+            keyC: 'val C',
+        },
+        children: [
+            {
+                name: 'Level 2: A',
+                attributes: {
+                    keyA: 'val A',
+                    keyB: 'val B',
+                    keyC: 'val C',
+                },
+            },
+            {
+                name: 'Level 2: B',
+            },
+        ],
+    },
+];
+
+const containerStyles = {
+    width: '100%',
+    height: '80vh',
+}
 
 const paperStyle = {
     height: "100%",
@@ -12,7 +43,8 @@ const paperStyle = {
     marginLeft: '20px',
     marginRight: '20px',
     marginTop: '20px',
-    marginBottom: '20px'
+    marginBottom: '20px',
+    width: '100%'
 };
 
 class Graph extends Component {
@@ -21,13 +53,26 @@ class Graph extends Component {
     };
 
     componentDidMount = async () => {
+        const dimensions = this.treeContainer.getBoundingClientRect();
+        this.setState({
+            translate: {
+                x: dimensions.width / 2,
+                y: dimensions.height / 2
+            }
+        });
 
     }
 
     render() {
         return (
             <Paper style={paperStyle}>
-                <Typography> This is a graph </Typography>
+                <div style={containerStyles} ref={tc => (this.treeContainer = tc)}>
+                    <Tree
+                        data={myTreeData}
+                        translate={this.state.translate}
+                        orientation={'vertical'}
+                    />
+                </div>
             </Paper>
         )
     }
