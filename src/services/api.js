00e4131f8533fd684318575cdcb5a82b150cd43d
Loughlin Fox <loughlinjfox@gmail.com>
2019-05-05 12:41:52 +1000

    Added services dir - mock file

---
+++ src/services/api.js
@@ -0,0 +1,15 @@
+
+const API_KEY = `FNSRA72PPZD837EAM6N6Q3ZU2EUKRYGPQ7`
+
+
+/*
+* a good place for all the api functions to be
+* then we can 
+* */
+
+
+function hello(name) {
+	return `Hello there ${name}`
+}
+
+export { hello }
