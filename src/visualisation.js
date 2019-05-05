aa94236b4fa5c9278543bcac8049085ba600446e
Lucas Bruck <lukabruck@gmail.com>
2019-05-05 13:51:06 +1000

    visulaisation data work

--- src/visualisation.js
+++ src/visualisation.js
@@ -4,16 +4,22 @@
  @author Lucas Bruck
  **/
 
+// import './mock-data.js';
+
+// API KEY
+// FNSRA72PPZD837EAM6N6Q3ZU2EUKRYGPQ7
+
+// All transactions
+// http://api.etherscan.io/api?module=account&action=txlist&address=0xddbd2b932c763ba5b1b7ae3b362eac3e8d40121a&startblock=0&endblock=99999999&sort=asc&apikey=FNSRA72PPZD837EAM6N6Q3ZU2EUKRYGPQ7
+
 
 /**
  * Graphnode
  * @param{id}: Integer. The sending node ID
- * @param{value}: Float. The value of the transaction
  */
 class GraphNode {
-    constructor(id, value) {
+    constructor(id) {
         this.id = id;
-        this.value = value;
 
         this.position = new Point();
     }
@@ -43,3 +49,61 @@ class Edge {
         this.id = id;
     }
 }
+
+/**
+ * Variables for nodes and edges
+ */
+
+// var nodes = [];
+// var edges = [];
+
+function processList() {
+    // var list = require('./mock-data.js');
+    var list = window.data;
+
+    let nodes = list.result.reduce(function(nodes, transaction) {
+        nodes[transaction.from] = {
+            id: transaction.from,
+            label: transaction.from,
+            x: Math.random(),
+            y: Math.random(),
+            size: 1,
+            color: "#000"
+        };
+
+        nodes[transaction.to] = {
+            id: transaction.to,
+            label: transaction.to,
+            x: Math.random(),
+            y: Math.random(),
+            size: 1,
+            color: "#000"
+        };
+
+        return nodes;
+    }, {});
+
+    let edges = list.result.reduce(function(edges, transaction) {
+        return edges.concat({
+            id: transaction.hash,
+            source: transaction.from,
+            target: transaction.to,
+            size: 1,
+            color: '#000'
+        });
+    }, []);
+
+    return [nodes, edges];
+}
+
+let [nodes, edges] = processList();
+
+let g = {
+    nodes: nodes,
+    edges: edges
+};
+
+s = new sigma({
+    graph: g,
+    container: "test"
+});
