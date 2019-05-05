92863d4a9ce03539b89595d68fc9a9629814118b
raymogg <raymondmogg@gmail.com>
2019-05-05 12:32:16 +1000

    Basic components fixed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import Graph from "./components/Graph.js"
 import AddressEntry from './components/AddressEntry';
+
 
 function App() {
   return (
     <div className="App">
-    <Graph />
-    <AddressEntry />
-      
+      <AppBar position="static" style={{ backgroundColor: "#2A2B2A" }}>
+        <Toolbar>
+          <Typography variant="h5" color="inherit" style={{ paddingRight: "50px" }}>
+            Transaction Visualizer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div className="mainContainer" style={{ paddingLeft: '25px', paddingRight: '25px', paddingTop: '15px'}}>
+        <AddressEntry />
+        <Graph />
+      </div>
+
     </div>
   );
 }
--- src/components/AddressEntry.js
+++ src/components/AddressEntry.js
@@ -1,17 +1,46 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField';
+
+const paperStyle = {
+    height: "100%",
+    marginTop: "5px",
+    flex: 1,
+    flexDirection: "row",
+    marginLeft: '20px',
+    marginRight: '20px',
+};
 
 class AddressEntry extends Component {
+
     state = {
-        
+        address: "0x0"
     };
 
     componentDidMount = async () => {
-        
+
+    }
+
+    handleChange = (text) => {
+        this.setState({address: text})
     }
 
     render() {
         return (
-            <h1> This is an address entry box </h1>
+            <div>
+                <Paper style={paperStyle}>
+                    <TextField
+                        id="addess-entry"
+                        label="Start Address"
+                        value={this.state.address}
+                        onChange={this.handleChange}
+                        margin="normal"
+                    />
+                    <Button> Search </Button>
+                </Paper>
+            </div>
         )
     }
 }
