14e47d29392e0094e32be5cffe053806b1bef49d
Raman Shalupau <raman.shalupau@gmail.com>
2020-10-07 14:22:02 +0800

    etherscan external links

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,145 +1,134 @@
-import React, {Component} from 'react';
-import {Graph} from 'react-d3-graph';
-import {DotLoader} from 'react-spinners';
-import Particles from 'react-particles-js';
-import sadFace from "../img/sad.png"
+import React, { Component } from 'react'
+import { Graph } from 'react-d3-graph'
+import { DotLoader } from 'react-spinners'
+import Particles from 'react-particles-js'
+import sadFace from '../img/sad.png'
 
-import windowSize from 'react-window-size';
-import "./AddressEntry.css";
-
+import windowSize from 'react-window-size'
+import './AddressEntry.css'
 
 const containerStyles = {
-    width: '100%',
-    height: '80vh',
+  width: '100%',
+  height: '80vh',
 }
-
 
 const myConfig = {
-    width: '1000',
-    height: '1000',
-    nodeHighlightBehavior: true,
-    linkHighlightBehavior: true,
-    automaticRearrangeAfterDropNode: true,
-    directed: false,
-    d3: {
-        gravity: -200,
-        linkStrength: 3
-    },
-    node: {
-        highlightStrokeColor: 'blue',
-        renderLabel: false,
-        //svg: 'https://upload.wikimedia.org/wikipedia/commons/0/05/Ethereum_logo_2014.svg'
-        //symbolType: 'diamond'
-    },
-    link: {
-        highlightColor: 'lightblue',
-        fontColor: "white",
-    }
+  width: '1000',
+  height: '1000',
+  nodeHighlightBehavior: true,
+  linkHighlightBehavior: true,
+  automaticRearrangeAfterDropNode: true,
+  directed: true,
+  d3: {
+    gravity: -200,
+    linkStrength: 3,
+  },
+  node: {
+    highlightStrokeColor: 'blue',
+    renderLabel: false,
+    //svg: 'https://upload.wikimedia.org/wikipedia/commons/0/05/Ethereum_logo_2014.svg'
+    //symbolType: 'diamond'
+  },
+  link: {
+    highlightColor: 'lightblue',
+    fontColor: 'white',
+  },
 }
-
 
 class CustomGraph extends Component {
-    state = {
-        nodeSize: 0,
-    };
+  state = {
+    nodeSize: 0,
+  }
 
-    setConfig(config) {
-        config.node.size = 5000 / this.props.graph.nodes.length;
-        config.directed = this.props.graph.directed
-        config.width = this.props.windowWidth
-        config.height = this.props.windowHeight
-        return config;
+  setConfig(config) {
+    config.node.size = 5000 / this.props.graph.nodes.length
+    config.directed = this.props.graph.directed
+    config.width = this.props.windowWidth
+    config.height = this.props.windowHeight
+    return config
+  }
+
+  componentDidUpdate = async () => {
+    // console.log('Graph componentDidUpdate')
+  }
+
+  getGraphRender = () => {
+    // console.log('Graph:getGraphRender: this.props.isLoading =', this.props.isLoading);
+    if (this.props.error) {
+      return (
+        <div className='loader'>
+          <img src={sadFace} alt='Uh Oh' style={{ width: '50px', height: '50px' }} />
+          <h2> This account has no transactions </h2>
+        </div>
+      )
     }
-
-    componentDidUpdate = async () => {
-      // console.log('Graph componentDidUpdate')
-    }
-
-    getGraphRender = () => {
-        // console.log('Graph:getGraphRender: this.props.isLoading =', this.props.isLoading);
-        if (this.props.error) {
-            return (
-                <div className="loader">
-                    <img src={sadFace} alt="Uh Oh" style={{width: '50px', height: '50px'}}/>
-                    <h2> This account has no transactions </h2>
-                </div>
-            )
-        }
-        if (!this.props.dataSet) {
-            if (this.props.isLoading === true) {
-                return (
-                    <div className="loader">
-                        <DotLoader
-                            sizeUnit={"px"}
-                            size={200}
-                            color={'lightgreen'}
-                            loading={this.props.isLoading}/>
-                    </div>
-                );
-            } else {
-                return (
-                        <div className="particles">
-                            <Particles
-                                params={{
-                                    "particles": {
-                                        "number": {
-                                            "value": 130
-                                        },
-                                        "size": {
-                                            "value": 3
-                                        },
-                                        "color": {
-                                          "value": "#90EE90"
-                                        }
-                                    },
-                                    "interactivity": {
-                                        "events": {
-                                            "onhover": {
-                                                "enable": true,
-                                                "mode": "grab"
-                                            },
-                                            "onclick": {
-                                                "enable": true,
-                                                "node": "push"
-                                            }
-                                        }
-                                    }
-                                }}
-                                width={this.props.windowWidth}
-                                height={this.props.windowHeight}
-                                style={{zIndex: 1}}/>
-                            <div className="landingPage">
-                                <h1>Ethereum Visualizer</h1>
-                                <h2>Search an Eth Address and trace its transactions</h2>
-                            </div>
-                        </div>
-                );
-            }
-        } else {
-            return (
-              <Graph
-                id='graph-id' // id is mandatory, if no id is defined rd3g will throw an error
-                data={this.props.graph}
-                config={this.setConfig(myConfig)}
-                style={{ width: '100%!important', height: '100vh!important' }}
-                onMouseOverNode={this.props.onHoverNode}
-                onClickGraph={this.props.onClickGraph}
-                onClickNode={this.props.onClickNode}
-                onDoubleClickNode={this.props.onDoubleClickNode}
-                onMouseOverLink={this.props.onMouseOverLink}
-              />
-            )
-        }
-    }
-
-    render() {
-
+    if (!this.props.dataSet) {
+      if (this.props.isLoading === true) {
         return (
-            <div>
-                {this.getGraphRender()}
-            </div>
+          <div className='loader'>
+            <DotLoader sizeUnit={'px'} size={200} color={'lightgreen'} loading={this.props.isLoading} />
+          </div>
         )
+      } else {
+        return (
+          <div className='particles'>
+            <Particles
+              params={{
+                particles: {
+                  number: {
+                    value: 130,
+                  },
+                  size: {
+                    value: 3,
+                  },
+                  color: {
+                    value: '#90EE90',
+                  },
+                },
+                interactivity: {
+                  events: {
+                    onhover: {
+                      enable: true,
+                      mode: 'grab',
+                    },
+                    onclick: {
+                      enable: true,
+                      node: 'push',
+                    },
+                  },
+                },
+              }}
+              width={this.props.windowWidth}
+              height={this.props.windowHeight}
+              style={{ zIndex: 1 }}
+            />
+            <div className='landingPage'>
+              <h1>Ethereum Visualizer</h1>
+              <h2>Search an Eth Address and trace its transactions</h2>
+            </div>
+          </div>
+        )
+      }
+    } else {
+      return (
+        <Graph
+          id='graph-id' // id is mandatory, if no id is defined rd3g will throw an error
+          data={this.props.graph}
+          config={this.setConfig(myConfig)}
+          style={{ width: '100%!important', height: '100vh!important' }}
+          onMouseOverNode={this.props.onHoverNode}
+          onClickGraph={this.props.onClickGraph}
+          onClickNode={this.props.onClickNode}
+          onDoubleClickNode={this.props.onDoubleClickNode}
+          onMouseOverLink={this.props.onMouseOverLink}
+        />
+      )
     }
+  }
+
+  render() {
+    return <div>{this.getGraphRender()}</div>
+  }
 }
 
-export default windowSize(CustomGraph);
+export default windowSize(CustomGraph)
--- src/components/NodeInfo.js
+++ src/components/NodeInfo.js
@@ -1,64 +1,70 @@
-import React, {Component} from 'react';
-
+import React, { Component } from 'react'
 
 export default class NodeInfo extends Component {
-    state = {}
+  state = {}
 
-    componentDidMount() {
-        this.props.nodes.sub(selectedNode => {
-            this.setState({ selectedNode })
-            this.convertCurrency(selectedNode)
-        })
-        this.props.currencyConversionRate.sub(currencyConversionRate => {
-            this.setState({ currencyConversionRate: currencyConversionRate })
-        })
-        this.props.currency.sub(currency => {
-            this.setState({currency: currency})
-        })
-    }
+  componentDidMount() {
+    this.props.nodes.sub(selectedNode => {
+      this.setState({ selectedNode })
+      this.convertCurrency(selectedNode)
+    })
+    this.props.currencyConversionRate.sub(currencyConversionRate => {
+      this.setState({ currencyConversionRate: currencyConversionRate })
+    })
+    this.props.currency.sub(currency => {
+      this.setState({ currency: currency })
+    })
+  }
 
-    convertCurrency(selectedNode) {
-        var newNetValue = selectedNode.netValue
-        var rate = this.state.currencyConversionRate
-        if (this.state.currency === "$") {
-            newNetValue = parseFloat(rate * newNetValue);
-        }
-        this.setState({netValue: newNetValue})
+  convertCurrency(selectedNode) {
+    var newNetValue = selectedNode.netValue
+    var rate = this.state.currencyConversionRate
+    if (this.state.currency === '$') {
+      newNetValue = parseFloat(rate * newNetValue)
     }
+    this.setState({ netValue: newNetValue })
+  }
 
-    render() {
-      return this.state.selectedNode
-          ? this.renderSelectedNode()
-          : NodeInfo.renderNoneSelected()
-    }
+  render() {
+    return this.state.selectedNode ? this.renderSelectedNode() : NodeInfo.renderNoneSelected()
+  }
 
-    renderSelectedNode() {
-        return (
-            <div className="selected-node">
-                <h4>{this.state.selectedNode.id}</h4>
-                <div className="row">
-                    <div>Outgoing Transactions</div>
-                    <div>{this.state.selectedNode.transactionsFromCount}</div>
-                </div>
-                <div className="row">
-                    <div>Incoming Transactions</div>
-                    <div>{this.state.selectedNode.transactionsToCount}</div>
-                </div>
-                <div className="row">
-                    <div>Node Net Value</div>
-                    <div>({this.state.currency}){this.state.netValue}</div>
-                </div>
-            </div>
-        )
-    }
+  renderSelectedNode() {
+    return (
+      <div className='selected-node'>
+        <h4>
+          <a
+            href={`http://etherscan.io/address/${this.state.selectedNode.id}`}
+            style={{ color: 'white' }}
+            target='_blank'>
+            {this.state.selectedNode.id}
+          </a>
+        </h4>
+        <div className='row'>
+          <div>Outgoing Transactions</div>
+          <div>{this.state.selectedNode.transactionsFromCount}</div>
+        </div>
+        <div className='row'>
+          <div>Incoming Transactions</div>
+          <div>{this.state.selectedNode.transactionsToCount}</div>
+        </div>
+        <div className='row'>
+          <div>Node Net Value</div>
+          <div>
+            ({this.state.currency}){this.state.netValue}
+          </div>
+        </div>
+      </div>
+    )
+  }
 
-    static renderNoneSelected() {
-        return (
-            <div className="selected-node">
-                <div>No Node selected yet</div>
-            </div>
-        )
-    }
+  static renderNoneSelected() {
+    return (
+      <div className='selected-node'>
+        <div>No Node selected yet</div>
+      </div>
+    )
+  }
 }
 
 //<div className="price-hover" onClick={this.onValueClick}>{this.state.selectedNode.currency}{this.state.selectedNode.netValue}</div>
